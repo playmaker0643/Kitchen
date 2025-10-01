2b2232aab1161db17767daa56ae0986da8ff3e1d
playmaker0643 <abdulhafiznasir0000@gmail.com>
2025-10-01 21:37:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
